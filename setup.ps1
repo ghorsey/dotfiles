5e3ab5d97a0eb17e34e9541aa78ebae24bb8bde1
ghorsey <jefedeltodos@outlook.com>
2024-12-26 15:17:27 -0800

    feat: added code to emit string to profile

--- setup.ps1
+++ setup.ps1
@@ -7,6 +7,11 @@ config config --local status.showUntrackedFiles no
 function config { git --git-dir=$HOME/.cfg/ --work-tree=$HOME $args }
 config config --local status.showUntrackedFiles no
 
+if (-Not (Select-String -Path $profile -Pattern 'function config { git --git-dir=$HOME/.cfg/ --work-tree=$HOME $args }'))
+{
+  Add-Content -Path $profile 'function config { git --git-dir=$HOME/.cfg/ --work-tree=$HOME $args }'
+}
+
 
 if (-Not (Get-Command starship -errorAction SilentlyContinue))
 {
