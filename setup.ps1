f918d3744754da322f8a73964453fb9e2dd3c8a5
ghorsey <jefedeltodos@outlook.com>
2024-12-26 22:03:23 -0800

    feat: added code to create an alias

--- setup.ps1
+++ setup.ps1
@@ -1,19 +1,52 @@
-if (-Not (Get-Command git -errorAction SilentlyContinue))
-{
-  winget install Git.Git
+function WinGetInstall {
+  param(
+    [string]$Command,
+    [string]$Package
+  )
+
+  if (-Not (Get-Command $Command -errorAction SilentlyContinue))
+  {
+    "winget install $Package"
+  }
 }
 
-config config --local status.showUntrackedFiles no
-function config { git --git-dir=$HOME/.cfg/ --work-tree=$HOME $args }
-config config --local status.showUntrackedFiles no
 
-if (-Not (Select-String -Path $profile -Pattern 'function config { git --git-dir=$HOME/.cfg/ --work-tree=$HOME $args }'))
-{
-  Add-Content -Path $profile 'function config { git --git-dir=$HOME/.cfg/ --work-tree=$HOME $args }'
+function WriteFile {
+  param(
+    [string]$Path,
+    [string]$Value
+  )
+
+  if (-Not (Get-Content $Path | Select-String -SimpleMatch -Pattern "$Value"))
+  {
+    Add-Content -Path $Path "$Value"
+  }
 }
 
+function CreateAlias {
+  param(
+    [string]$Alias,
+    [string]$Command,
+    [string]$Path
+  )
+
+  WriteFile -Path $Path -Value "function $Alias { $Command $args }"
+}
+
+
+WinGetInstall -Command git -Package Git.Git
+WinGetInstall -Command rustup -Package Rustlang.Rustup
+
+function config { git --git-dir=$HOME/.cfg/ --work-tree=$HOME $args } # Create a function for within this script
+config config --local status.showUntrackedFiles no
+
+CreateAlias -Alias "config" -Path $profile -Command 'git --git-dir=$HOME/.cfg/ --work-tree=$HOME $args'
+#WriteFile -Path $profile -Value 'function config { git --git-dir=$HOME/.cfg/ --work-tree=$HOME $args }'
 
 if (-Not (Get-Command starship -errorAction SilentlyContinue))
 {
   "starship exists"
 }
+
+
+. $profile
