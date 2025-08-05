b73958f38279403936d2a2ecb5a1671941968120
Geoff <geoff.horsey@outlook.com>
2025-08-05 00:14:41 -0700

    Update config.fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,7 +1,8 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
-
+source "$HOME/.cargo/env.fish"  # For fish
 starship init fish | source
+set -gx EDITOR ~/.local/share/bob/nvim-bin/nvim
 
 set fish_greeting ""
