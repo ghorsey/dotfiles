92b5ceb4e91c0ba2d8da57a21041761bfb91b10b
ghorsey <geoff.horsey@outlook.com>
2025-08-18 23:26:51 -0700

    feat: got basic setup working

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -2,6 +2,9 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 starship init fish | source
+bob complete fish | source
 set -gx EDITOR ~/.local/share/bob/nvim-bin/nvim
+
+fish_add_path -a ~/.local/share/bob/nvim-bin
 
 set fish_greeting ""
