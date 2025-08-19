f98ce5d2c2ae1ac5d1b6b5f3655e1679866625bc
ghorsey <geoff.horsey@outlook.com>
2025-08-18 23:39:45 -0700

    feat: added pay-respects and zoxide

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,10 +1,16 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
-starship init fish | source
-bob complete fish | source
 set -gx EDITOR ~/.local/share/bob/nvim-bin/nvim
 
 fish_add_path -a ~/.local/share/bob/nvim-bin
 
 set fish_greeting ""
+
+starship init fish | source
+
+bob complete fish | source
+
+pay-respects fish --alias | source
+
+zoxide init fish | source
