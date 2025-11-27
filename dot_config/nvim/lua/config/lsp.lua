fb67b05b0824a4e25c071bbc797f187f64ed5af8
ghorsey <geoff.horsey@outlook.com>
2025-11-26 20:58:44 -0600

    feat: added <C-space> to invoke autocomplete when in insert mode

--- dot_config/nvim/lua/config/lsp.lua
+++ dot_config/nvim/lua/config/lsp.lua
@@ -4,6 +4,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
     local client = vim.lsp.get_client_by_id(ev.data.client_id)
     if client:supports_method('textDocument/completion') then
       vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+      vim.keymap.set('i', '<C-space>', vim.lsp.completion.get, { desc = 'trigger autocomplete' })
     end
   end,
 })
