df39821d86a6614b43512937a666c2c94f8e800f
ghorsey <geoff.horsey@outlook.com>
2026-02-14 11:37:08 -0800

    feat: added js debugging

--- dot_config/nvim/lua/plugins/dap.lua
+++ dot_config/nvim/lua/plugins/dap.lua
@@ -85,6 +85,18 @@ return {
           },
         },
       }
+
+      dap.adapters[adapterType] = function(cb, config)
+        local nativeAdapter = dap.adapters[pwaType]
+
+        config.type = pwaType
+
+        if type(nativeAdapter) == "function" then
+          nativeAdapter(cb, config)
+        else
+          cb(nativeAdapter)
+        end
+      end
     end
 
     local enter_launch_url = function()
@@ -99,6 +111,50 @@ return {
           end
         end)
       end)
+    end
+
+    for _, language in ipairs({ "typescript", "javascript", "vue", "angular", "typescriptreact", "javscriptreact" }) do
+      dap.configurations[language] = {
+        {
+          type = "pwa-node",
+          request = "launch",
+          name = "Launch Node (dap)",
+          program = "${file}",
+          cwd = "${workspaceFolder}",
+        },
+        {
+          type = "pwa-node",
+          request = "attach",
+          name = "Attach Node (dap)",
+          processId = require("dap.utils").pick_process,
+          cwd = "${workspaceFolder}",
+        },
+        {
+          type = "pwa-node",
+          request = "launch",
+          name = "Launch TS Node (dap)",
+          program = "${file}",
+          cwd = "$workspaceFolder",
+          runtimeArgs = { "-r", "ts-node/register" },
+        },
+        {
+          type = "pwa-chrome",
+          request = "launch",
+          name = "Launch Chrome (Dap)",
+          url = enter_launch_url,
+          webRoot = "${workspaceFolder}",
+          sourceMaps = true,
+        },
+        {
+          type = "pwa-edge",
+          request = "launch",
+          name = "Launch Edge (Dap)",
+          url = enter_launch_url(),
+          webRoot = "${workspaceFolder}",
+          sourceMaps = true,
+        },
+      }
+
     end
 
 
