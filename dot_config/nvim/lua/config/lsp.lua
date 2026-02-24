8fc497c52b3312886d00baa23b2885f85f808453
Geoff <geoff.horsey@outlook.com>
2026-02-24 13:20:56 -0800

    Comment out LSP completion settings
    Comment out the completion enable and keymap settings for LSP.

--- dot_config/nvim/lua/config/lsp.lua
+++ dot_config/nvim/lua/config/lsp.lua
@@ -2,9 +2,9 @@ vim.api.nvim_create_autocmd('LspAttach', {
   callback = function(ev)
     local client = vim.lsp.get_client_by_id(ev.data.client_id)
     if client:supports_method('textDocument/completion') then
-      vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+      --vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
       --vim.keymap.set('i', '<C-space>', vim.lsp.completion.get, { desc = 'trigger autocomplete' })
-      vim.keymap.set('i', '<C-space>', function() require('mini.completion').complete() end, { desc = 'trigger autocomplete' })
+      --vim.keymap.set('i', '<C-space>', function() require('mini.completion').complete() end, { desc = 'trigger autocomplete' })
       vim.keymap.set(
         'i',
         '<Tab>',
