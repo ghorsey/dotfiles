94c241a0f3d111d765fea2dbdfdb7b8e967e1c43
Geoff <geoff.horsey@outlook.com>
2026-02-24 12:08:54 -0800

    Update autocomplete key mapping for LSP

--- dot_config/nvim/lua/config/lsp.lua
+++ dot_config/nvim/lua/config/lsp.lua
@@ -3,7 +3,8 @@ vim.api.nvim_create_autocmd('LspAttach', {
     local client = vim.lsp.get_client_by_id(ev.data.client_id)
     if client:supports_method('textDocument/completion') then
       vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
-      vim.keymap.set('i', '<C-space>', vim.lsp.completion.get, { desc = 'trigger autocomplete' })
+      --vim.keymap.set('i', '<C-space>', vim.lsp.completion.get, { desc = 'trigger autocomplete' })
+      vim.keymap.set('i', '<C-space>', function() require('mini.completion').complete() end, { desc = 'trigger autocomplete' })
       vim.keymap.set(
         'i',
         '<Tab>',
