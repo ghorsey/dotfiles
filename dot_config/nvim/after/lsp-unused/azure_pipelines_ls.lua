3443b7023340560e6ca982031323d6a6e42879ec
Geoff <geoff.horsey@outlook.com>
2026-02-25 18:00:15 -0800

    Update YAML schemas for Azure Pipelines LSP

--- dot_config/nvim/after/lsp-unused/azure_pipelines_ls.lua
+++ dot_config/nvim/after/lsp-unused/azure_pipelines_ls.lua
@@ -1,11 +1,17 @@
 return {
+  root_markers = { 'azure-pipelines.yml', '.azure-pipeline', '.azure-pipelines' },
   settings = {
     yaml = {
       schemas = {
-         ["https://raw.githubusercontent.com/microsoft/azure-pipelines-vscode/master/service-schema.json"] = {
-          ".azure-pipelines/**/*.y*l",
+        ["https://raw.githubusercontent.com/microsoft/azure-pipelines-vscode/master/service-schema.json"] = {
+            "/azure-pipeline*.y*l",
+            "/*.azure*",
+            "Azure-Pipelines/**/*.y*l",
+            "Pipelines/*.y*l",
+            "/*-pipeline.y*l",
         },
       },
     },
   },
 }
+
