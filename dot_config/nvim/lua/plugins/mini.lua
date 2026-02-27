152946551058038f418f58763d3a2cbe2cec7a5b
Geoff <geoff.horsey@outlook.com>
2026-02-27 11:32:12 -0800

    Configure mini.completion window dimensions and border
    Add custom window settings for mini.completion

--- dot_config/nvim/lua/plugins/mini.lua
+++ dot_config/nvim/lua/plugins/mini.lua
@@ -5,6 +5,10 @@ return {
     require('mini.surround').setup()
     require('mini.statusline').setup()
     require('mini.completion').setup({
+      window = {
+        info = { height = 25, width = 80, border = "shadow" },
+        signature = { height = 25, width = 80, border = "shadow" },
+      },
       mappings = {
         force_twostep = "<C-@>",
         force_fallback = "<leader><C-@>",
