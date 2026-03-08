4fed903383634347cc0870895e96ede50d7dd4b9
Geoff <geoff.horsey@outlook.com>
2026-03-08 08:08:40 -0700

    Update undotree.lua

--- dot_config/nvim/lua/plugins/undotree.lua
+++ dot_config/nvim/lua/plugins/undotree.lua
@@ -1,9 +1,6 @@
 return {
   "jiaoshijie/undotree",
-  opts = {
-    -- your options
-  },
-  keys = { -- load the plugin only when using it's keybinding:
+  keys = {
     { "<leader>u", "<cmd>lua require('undotree').toggle()<cr>" },
   },
 }
