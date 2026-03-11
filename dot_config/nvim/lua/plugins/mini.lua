a3876aa3a6a344ac7ad36fa147a8e7db037ce967
Geoff <geoff.horsey@outlook.com>
2026-03-11 16:17:54 -0700

    Disable mini.completion setup
    Comment out mini.completion setup to prevent fallback menu issues.

--- dot_config/nvim/lua/plugins/mini.lua
+++ dot_config/nvim/lua/plugins/mini.lua
@@ -31,31 +31,31 @@ return {
       },
     })
     require('mini.statusline').setup()
-    require('mini.completion').setup({
-      window = {
-        info = { height = 25, width = 80, border = "rounded" },
-        signature = { height = 25, width = 80, border = "rounded" },
-      },
-      fallback_action = function()  end, -- this was causing strange fallback menu
-      lsp_completion = {
-        process_items = function(items)
-          local CompletionItemKind = vim.lsp.protocol.CompletionItemKind
-          local filtered = {}
+    -- require('mini.completion').setup({
+    --   window = {
+    --     info = { height = 25, width = 80, border = "rounded" },
+    --     signature = { height = 25, width = 80, border = "rounded" },
+    --   },
+    --   fallback_action = function()  end, -- this was causing strange fallback menu
+    --   lsp_completion = {
+    --     process_items = function(items)
+    --       local CompletionItemKind = vim.lsp.protocol.CompletionItemKind
+    --       local filtered = {}
 
-          for _, item in ipairs(items) do
-            if item.kind ~= CompletionItemKind.Text then
-              table.insert(filtered, item)
-            end
-          end
+    --       for _, item in ipairs(items) do
+    --         if item.kind ~= CompletionItemKind.Text then
+    --           table.insert(filtered, item)
+    --         end
+    --       end
 
-          return filtered
-        end,
-      },
-      -- version
-      -- mappings = {
-      --   force_twostep = "<C-@>",
-      --   force_fallback = "<leader><C-@>",
-      -- },
-    })
+    --       return filtered
+    --     end,
+    --   },
+    --   -- version
+    --   -- mappings = {
+    --   --   force_twostep = "<C-@>",
+    --   --   force_fallback = "<leader><C-@>",
+    --   -- },
+    -- })
   end,
 }
