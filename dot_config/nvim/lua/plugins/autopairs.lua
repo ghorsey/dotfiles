8518268ff95fbaeaf9ff322b40538844a04fcb2f
Geoff <geoff.horsey@outlook.com>
2026-03-13 11:20:51 -0700

    Enhance nvim-autopairs configuration
    Added custom rules for nvim-autopairs to handle parentheses and <CR> key.

--- dot_config/nvim/lua/plugins/autopairs.lua
+++ dot_config/nvim/lua/plugins/autopairs.lua
@@ -1,7 +1,25 @@
 return {
     'windwp/nvim-autopairs',
     event = "InsertEnter",
-    config = true
+    config = true,
     -- use opts = {} for passing setup options
     -- this is equivalent to setup({}) function
+    init = function()
+        local npairs = require("nvim-autopairs")
+        local Rule = require("nvim-autopairs.rule")
+
+        npairs.add_rules({
+            Rule("(", ")")
+                :with_cr(function()
+                    -- allow autopairs to handle <CR>
+                    return true
+                end)
+                :replace_endpair(function(opts)
+                    -- opts.prev_char is the text before cursor
+                    -- we return what should appear AFTER the cursor
+                    return ")"
+                end)
+                :use_key("<cr>")
+        })
+    end
 }
