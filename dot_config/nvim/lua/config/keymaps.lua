5f5195e4a8ec55d1c9898e50530e4968833660b5
Geoff <geoff.horsey@outlook.com>
2026-03-13 09:43:53 -0700

    Change save all buffers shortcut to Ctrl+M+S

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -5,8 +5,12 @@ vim.g.maplocalleader = " "                           -- Set local leader key (NE
 -- Save buffers
 vim.keymap.set("n", "<C-s>", "<CMD>w<CR>", { desc = "Save the current buffer", silent = true, })
 vim.keymap.set("i", "<C-s>", "<ESC><CMD>w<CR>a", { desc = "Save the current buffer", silent = true, })
-vim.keymap.set( "n","<C-S-s>", "<CMD>wa<CR>", { desc = "Save all open buffers", silent = true, })
-vim.keymap.set("i", "<C-S-s>", "<ESC><CMD>wa<CR>a", { desc = "Save all open buffers", silent = true, })
+vim.keymap.set( "n","<C-M-s>", "<CMD>wa<CR>", { desc = "Save all open buffers", silent = true, })
+vim.keymap.set("i", "<C-M-s>", "<ESC><CMD>wa<CR>a", { desc = "Save all open buffers", silent = true, })
+
+-- Undo
+vim.keymap.set("n", "<C-u>", "<CMD>u<CR>", { desc = "Undo the last action", silent = true })
+vim.keymap.set("i", "<C-u>", "<ESC><CMD>u<CR>a", desc = "Undo the last action", silent = true })
 
 -- Quit neovim
 vim.keymap.set("n", "<C-q>", "<CMD>q<CR>", { desc = "Close the current window", silent = true})
