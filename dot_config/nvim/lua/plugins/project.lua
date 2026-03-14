5ff43086b05f548830723c1b435a8da3b8bc7a00
ghorsey <geoff.horsey@outlook.com>
2026-03-14 11:01:00 -0700

    feat: added project.nvim and a few packages

---
+++ dot_config/nvim/lua/plugins/project.lua
@@ -0,0 +1,12 @@
+return {
+  'DrKJeff16/project.nvim',
+  dependencies = { -- OPTIONAL. Choose any of the following
+    'folke/snacks.nvim',
+  },
+  opts = {
+    patterns = {
+      "slnx",
+      "package.json",
+    },
+  },
+}
