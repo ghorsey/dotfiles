064892be90a2f1ca9b4cb3d6a8ba9bc3785538a2
Geoff <geoff.horsey@outlook.com>
2026-07-02 14:24:34 -0700

    Update lsp.lua

--- dot_config/nvim/lua/config/lsp.lua
+++ dot_config/nvim/lua/config/lsp.lua
@@ -6,35 +6,11 @@ vim.api.nvim_create_autocmd('LspAttach', {
       Snacks.notify.warn("The client " .. ev.data.client_id .. " was not found")
       return
     end
-    
+
     -- vim.lsp.config('*', {capabilities = MiniCompletion.get_lsp_capabilities()})
     vim.lsp.config('*', { capabilities = require('blink.cmp').get_lsp_capabilities() })
-    
-    vim.lsp.inlay_hint.enable(true)      
-    
-    -- Do I need this?
-    if client:supports_method('textDocument/completion') then
-      vim.keymap.set(
-        'i',
-        '<Tab>',
-        function()
-          if vim.fn.pumvisible() == 1 then
-            local selected = vim.fn.complete_info().selected
-            if selected == -1 then
-              -- No item selected: select first item, then confirm
-              return "<C-n><C-y>"
-            else
-              return "<C-y>"
-            end
-          end
-          return "<Tab>"
-        end,
-        {
-          noremap = true,
-          expr = true,
-          desc = 'insert highlighted item'
-        })
-    end
+
+    vim.lsp.inlay_hint.enable(true)
   end,
 })
 
