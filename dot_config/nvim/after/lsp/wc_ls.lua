b68fe333d15036ab3667ad605a07a8745a8bbeb1
ghorsey <geoff.horsey@outlook.com>
2026-07-11 11:35:03 -0700

    feat: configured wc_ls from lspconfig sample

--- dot_config/nvim/after/lsp/wc_ls.lua
+++ dot_config/nvim/after/lsp/wc_ls.lua
@@ -1,3 +1,30 @@
-local M = {}
+local M = {
+  init_options = { hostInfo = 'neovim' },
+  cmd = { 'wc-language-server', '--stdio' },
+  filetypes = {
+    'html',
+    'javascriptreact',
+    'typescriptreact',
+    'astro',
+    'svelte',
+    'vue',
+    'markdown',
+    'mdx',
+    'javascript',
+    'typescript',
+    'css',
+    'scss',
+    'less',
+  },
+  root_markers = {
+    'wc.config.js',
+    'wc.config.ts',
+    'wc.config.mjs',
+    'wc.config.cjs',
+    'custom-elements.json',
+    'package.json',
+    '.git',
+  },
+}
 
 return M
